7b7edd2d3cbcf6ae930c9aa2a4020b88d83f72e7
phuan2309 <phudang2206@gmail.com>
2026-04-25 11:14:12 +0700

    fix v1

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -23,7 +23,7 @@ function Home2() {
               <i>
                 <b className="purple">
                   {" "}
-                  JavaScript, C++, Rust, Node.js, and Java{" "}
+                  JavaScript, Node.js, and Java{" "}
                 </b>
               </i>
               — and I enjoy working across both backend and frontend stacks.
